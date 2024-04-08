f8d7d6bdcc471e5639d6c97c38b1eb269b1222b2
Yamijke <serhei.knyazev@gmail.com>
2024-04-08 16:37:42 +0300

    1. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,14 +2,19 @@ package ru.job4j.tracker;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
+@Entity
+@Table(name = "items")
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NonNull
     private String name;
