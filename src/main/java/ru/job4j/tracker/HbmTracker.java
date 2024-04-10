e6b74b9c315dc56fa01892badf08063d9d66f100
Yamijke <serhei.knyazev@gmail.com>
2024-04-10 22:57:10 +0300

    3. HbmTracker [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,123 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery("Update Item SET name = :fName, created = :created where id = :fid")
+                    .setParameter("fName", item.getName())
+                    .setParameter("created", Timestamp.valueOf(item.getCreated()))
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return false;
+    }
+
+    @Override
+    public void delete(int id) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        Session session = sf.openSession();
+        List<Item> rsl = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery("from Item", Item.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        List<Item> rsl = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery("from Items where name = :key", Item.class)
+                    .setParameter("key", key)
+                    .getResultList();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sf.openSession();
+        var rsl = new Item();
+        try {
+            rsl = session.createQuery("from Items where id = :id", Item.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
