7740a025a52f753a7a98028b6b65c0bdb7900c9e
Yamijke <serhei.knyazev@gmail.com>
2024-05-06 18:46:14 +0300

    1. Интеграционное тестирование с Hibernate [#6875 #494777]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -52,9 +52,9 @@ public class Item {
             return false;
         }
         Item item = (Item) o;
-        return id == item.id &&
-                Objects.equals(name, item.name) &&
-                Objects.equals(created, item.created);
+        return id == item.id
+                && Objects.equals(name, item.name)
+                && Objects.equals(created, item.created);
     }
 
     @Override
